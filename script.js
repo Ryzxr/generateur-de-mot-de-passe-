934cab6aedde58585a45225097719cd1bbd1f6aa
Ryzxr <127143525+Ryzxr@users.noreply.github.com>
2023-03-07 10:48:21 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+const generatePassword = () => {
+  const length = 12;
+  const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset[randomIndex];
+  }
+  return password;
+};
+
+const generatePasswordButton = document.getElementById("generate-password");
+const passwordInput = document.getElementById("password");
+
+generatePasswordButton.addEventListener("click", () => {
+  const password = generatePassword();
+  passwordInput.value = password;
+});
